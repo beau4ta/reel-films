28c74d7d6c9185655c55ccde7644085a4fa45efa
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-07-20 17:31:12 -0400

    Routing in App.js and Login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Organization from "./pages/Organization";
-import SignIn from "./pages/SignIn";
-import OrgProfile from "./pages/OrgProfile";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
-import Signup from "./pages/Signup";
 import "./App.css";
-
 
 function App() {
   return (
     <Router>
-      <div className="wrapper">
+      <div className="App">
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path={["/api/charity/id/:id", "/api/charity/:id"]}
-            component={Organization}
-          />
-          <Route
-            exact
-            path={["/SignIn", "/api/charity/SignIn", "/profile/SignIn"]}
-            component={SignIn}
-          />
+          <Route exact path={["/Login", "/api/movies/Login", "/profile/Login"]} component={Login}/>
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/profile/:id" component={OrgProfile} />
+          <Route exact path="/movies/:id" component={Movies} />
           <Route component={NoMatch} />
         </Switch>
         <Footer />
