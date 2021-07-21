922b2075a0ee975e54faa48bb4d8398e299d8e07
Griffin Pristic <gpristic@gmail.com>
2021-07-20 21:00:43 -0400

    redo

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,5 +7,11 @@ const APIKEY = 'b4f70639'
 export default {
     getMovies: function(movie) {
     return axios.get(BASEURL + APIKEY + '&' + movie )
-}
+},
+createUser: function(user) {
+    return axios.post("/api/movies/register", user)
+  },
+  signIn: function(user) {
+    return axios.post("/api/movies/signin", user)
+  },
 }
