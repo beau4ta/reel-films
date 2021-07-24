9ccb1c0317de89ca332071a4667cd75b710cd2e8
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-07-24 11:27:41 -0400

    CSS edits

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import SearchForm from '../components/search/searchForm';
-import AllResults from '../components/results/allResults';
-import API from '../utils/API';
+import React, { Component } from "react";
+import SearchForm from "../components/search/searchForm";
+import AllResults from "../components/results/allResults";
+import API from "../utils/API";
 
 class Home extends Component {
-    state = {
-        search: '',
-        movies: [],
+  state = {
+    search: "",
+    movies: [],
+  };
 
-    }
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value });
+  };
 
-    handleInputChange = event => {
-        this.setState({ search: event.target.value })
-      }
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    API.getMovies(this.state.search)
+      .then((res) => {
+        console.log(res);
+        this.setState({ movies: [res.data] });
+      })
+      .catch((err) => console.log(err));
+  };
 
-      handleFormSubmit = event => {
-        event.preventDefault();
-        API.getMovies(this.state.search)
-        .then(res => {
-            console.log(res)
-            this.setState({ movies: [res.data] })
-        })
-        .catch(err => console.log(err));
-    };
-
-
-    render(){
-        return(
-            <div>
-                <SearchForm
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit} 
-                />
-                <AllResults 
-                movies={this.state.movies}
-                saveMovies={movie => this.saveMovie(movie)}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <SearchForm
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <AllResults
+          movies={this.state.movies}
+          saveMovies={(movie) => this.saveMovie(movie)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
