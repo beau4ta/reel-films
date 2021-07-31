d84318c0086ba3a65aaf16a92f34fe5690e7b2fd
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-07-31 11:02:11 -0400

    updated css in search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,29 +6,29 @@ const BASEURL = "http://www.omdbapi.com/?apikey=";
 const APIKEY = "23c335db";
 
 export default {
-getSearchMovies: function(search) {
-  return axios.get(BASEURL + APIKEY + '&s=' + search) 
-},
+  getSearchMovies: function (search) {
+    return axios.get(BASEURL + APIKEY + '&s=' + search);
+  },
 
-getMovies: function () {
+  getMovies: function () {
     return axios.get('/api/movies');
-},
-
-saveMovie: function (savedMovies) {
-  return axios.post('/api/movies/', savedMovies);
-},
-
-deleteMovie: function (id) {
-  return axios.delete('/api/movies/' + id)
-},
-
-createUser: function(user) {
-    return axios.post("/api/user/signup", user)
   },
 
-signIn: function(user) {
-    return axios.post("/api/user/signin", user)
+  saveMovie: function (savedMovies) {
+    return axios.post('/api/movies/', savedMovies);
   },
-}
+
+  deleteMovie: function (id) {
+    return axios.delete('/api/movies/' + id);
+  },
+
+  createUser: function (user) {
+    return axios.post("/api/user/signup", user);
+  },
+
+  signIn: function (user) {
+    return axios.post("/api/user/signin", user);
+  },
+};
 
 
